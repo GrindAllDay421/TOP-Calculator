7954a6e68ca9efb41c349919e6aa2dfffb7e785a
GrindAllDay421 <codiejaworsky@gmail.com>
2022-02-22 23:31:34 -0700

    added buttons for numbers and operators and started work on css

--- Calculator.js
+++ Calculator.js
@@ -13,3 +13,20 @@ const multiply = function(num1, num2) {
 const divide = function(num1, num2) {
   return num1 / num2;
 }
+
+const operate = function(operator, num1, num2) {
+  switch(operator) {
+    case 'add':
+      return add(num1, num2);
+      break;
+    case 'subtract':
+      return subtract(num1, num2);
+      break;
+    case 'multiply':
+      return multiply(num1, num2);
+      break;
+    case 'divide':
+      return divide(num1, num2);
+      break;
+  }
+}
